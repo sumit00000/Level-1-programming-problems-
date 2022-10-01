c57c9bc3523192eb5e7cc777e3ce44e90d1774bd
sumit00000 <76873515+sumit00000@users.noreply.github.com>
2022-10-01 11:38:08 +0530

    Break and Pass Statements programs

---
+++ Break and Pass kewords.py
@@ -0,0 +1,15 @@
+
+#TODO Continue Keyword
+
+for l in "helloworld":
+    if l == 'o':
+        continue
+    print("value is: ", l)
+
+#TODO Pass Keyword
+
+for l in "Helloworld":
+    if l == 'o':
+        pass
+        print("I will do this later")
+    print("value is: ", l)   
